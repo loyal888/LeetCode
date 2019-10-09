f02187d72aeeea67fc9d18ef97a7a290376e0c32
loyal888 <yunsong.hu@hand-china.com>
2019-10-09 11:09:25 +0800

    33. 搜索旋转排序数组

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -442,6 +442,24 @@ public class LeetCode {
         return  ans;
     }
 
+    /**
+     * 33. 搜索旋转排序数组
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int lo = 0, hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if ((nums[0] > target) ^ (nums[0] > nums[mid]) ^ (target > nums[mid]))
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo == hi && nums[lo] == target ? lo : -1;
+    }
+
     public static void main(String[] args) {
 longestValidParentheses("()(())");
 
