ae27536c1327466037fc3b1198b5f319efe671aa
loyal888 <yunsong.hu@hand-china.com>
2019-10-10 10:26:27 +0800

    46. 全排列

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -570,29 +570,34 @@ public class LeetCode {
         return ans;
     }
 
-//    public static List<List<Integer>> permute(int[] nums) {
-//
-//        List<List<Integer>> res = new ArrayList<>();
-//        int[] visited = new int[nums.length];
-//        backtrack(res, nums, new ArrayList<Integer>(), visited);
-//        return res;
-//
-//    }
-//
-//    private static void backtrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> tmp, int[] visited) {
-//        if (tmp.size() == nums.length) {
-//            res.add(new ArrayList<>(tmp));
-//            return;
-//        }
-//        for (int i = 0; i < nums.length; i++) {
-//            if (visited[i] == 1) continue;
-//            visited[i] = 1;
-//            tmp.add(nums[i]);
-//            backtrack(res, nums, tmp, visited);
-//            visited[i] = 0;
-//            tmp.remove(tmp.size() - 1);
-//        }
-//    }
+    /**
+     * 46. 全排列
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        int[] visited = new int[nums.length];
+        backtrack(res, nums, new ArrayList<Integer>(), visited);
+        return res;
+
+    }
+
+    private static void backtrack(List<List<Integer>> res, int[] nums, ArrayList<Integer> tmp, int[] visited) {
+        if (tmp.size() == nums.length) {
+            res.add(new ArrayList<>(tmp));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i] == 1) continue;
+            visited[i] = 1;
+            tmp.add(nums[i]);
+            backtrack(res, nums, tmp, visited);
+            visited[i] = 0;
+            tmp.remove(tmp.size() - 1);
+        }
+    }
 
     public static void main(String[] args) {
         int[] array = {1,2,3};
