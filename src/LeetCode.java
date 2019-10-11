2dfdd9035a9027a8af435b38521aefabbd25ce0a
loyal888 <yunsong.hu@hand-china.com>
2019-10-11 11:04:59 +0800

    72. 编辑距离

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -796,6 +796,7 @@ public class LeetCode {
 
     /**
      * 62. 不同路径
+     *
      * @param m
      * @param n
      * @return
@@ -815,50 +816,84 @@ public class LeetCode {
     public int minPathSum(int[][] grid) {
         int M = grid[0].length;
         int N = grid.length;
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(i==0&&j==0){
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (i == 0 && j == 0) {
                     continue;
                 }
-                if(i==0&&j>0){
-                    grid[0][j] += grid[0][j-1];
+                if (i == 0 && j > 0) {
+                    grid[0][j] += grid[0][j - 1];
                     continue;
                 }
-                if(j==0&&i>0){
-                    grid[i][0] += grid[i-1][0];
+                if (j == 0 && i > 0) {
+                    grid[i][0] += grid[i - 1][0];
                     continue;
                 }
-                grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
             }
         }
-        return grid[N-1][M-1];
+        return grid[N - 1][M - 1];
     }
 
     /**
      * 70. 爬楼梯
+     *
      * @param n
      * @return
      */
     public int climbStairs(int n) {
-        if(n == 1){return 1;}
-        if(n == 2){return 2;}
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
 
         int[] dp = new int[n];
         dp[0] = 1;
         dp[1] = 2;
 
-        for(int i=2;i<n;i++){
-            dp[i] = dp[i-1] + dp[i-2];
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        return dp[n-1];
+        return dp[n - 1];
     }
+
+    /**
+     * 72. 编辑距离
+     *
+     * @param word1
+     * @param word2
+     * @return
+     */
+    public int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        // 第一行
+        for (int j = 1; j <= n2; j++) dp[0][j] = dp[0][j - 1] + 1;
+        // 第一列
+        for (int i = 1; i <= n1; i++) dp[i][0] = dp[i - 1][0] + 1;
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j - 1], dp[i][j - 1]), dp[i - 1][j]) + 1;
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+
 
     public static void main(String[] args) {
 //        String[] array = {"abc", "acb", "aq", "qa"};
 
 
-        int[][] array = {{1, 2,5}, {3, 2,1}};
-        int i1 = new LeetCode().minPathSum(array);
+        int[][] array = {{1, 2, 5}, {3, 2, 1}};
+        int i1 = new LeetCode().minDistance("horse", "ros");
         int i;
     }
 
