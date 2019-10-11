ee71ece741543676379d22839691bddaf2c1e860
loyal888 <yunsong.hu@hand-china.com>
2019-10-11 11:24:11 +0800

    75. 颜色分类

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -894,7 +894,7 @@ public class LeetCode {
         int p0 = 0,curr = 0;
         int p2 = nums.length-1;
         int tmp;
-        while (curr <p2){
+        while (curr <= p2){
             if(nums[curr] == 0){
                 tmp = nums[p0];
                 nums[p0++] = nums[curr];
