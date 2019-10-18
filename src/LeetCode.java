3bea7c753a2adb46f8ae76a10b72d1e7ea550239
loyal888 <yunsong.hu@hand-china.com>
2019-10-18 12:50:17 +0800

    94. 二叉树的中序遍历

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -17,6 +17,19 @@ public class LeetCode {
         }
     }
 
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+
     public static int myAtoi(String str) {
         // 去除空格
         str = str.trim();
@@ -1007,6 +1020,30 @@ public class LeetCode {
         }
         List<Integer> pre = new ArrayList<>();
         find(nums, 0, pre);
+        return res;
+    }
+
+
+    /**
+     * 94. 二叉树的中序遍历
+     * @param root
+     * @return
+     * 时间复杂度：O(n)。
+     * 空间复杂度：O(n))。
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
         return res;
     }
 
