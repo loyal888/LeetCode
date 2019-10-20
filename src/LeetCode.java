884009342e74447bb55c625e175b2bae339896b4
loyal888 <yunsong.hu@hand-china.com>
2019-10-20 13:27:48 +0800

    101. 对称二叉树

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1095,6 +1095,20 @@ public class LeetCode {
         return true;
     }
 
+    /**
+     * 101. 对称二叉树
+     * @param root
+     * @return
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+    public boolean isMirror(TreeNode t1,TreeNode t2){
+        if(t1 == null && t2 == null){return true;}
+        if(t1 == null || t2 == null){return false;}
+        return (t1.val == t2.val) && isMirror(t1.left,t2.right) && isMirror(t2.left,t1.right);
+    }
+
 
     public static void main(String[] args) {
 //        String[] array = {"abc", "acb", "aq", "qa"};
