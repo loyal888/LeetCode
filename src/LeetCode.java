eafbef662aa8483034a30577fea76f0751d18da3
loyal888 <yunsong.hu@hand-china.com>
2019-10-24 11:00:03 +0800

    200. 岛屿数量

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1544,6 +1544,7 @@ public class LeetCode {
 
     /**
      * 169. 求众数
+     *
      * @param nums
      * @return
      */
@@ -1561,59 +1562,111 @@ public class LeetCode {
 
     /**
      * 198. 打家劫舍
-     * @param num
      *
+     * @param num
      * @return
      */
-    public int rob(int[] num){
+    public int rob(int[] num) {
         // 前一个值
         int preMax = 0;
         // 当前最大值
         int currMax = 0;
-        for(int x:num){
+        for (int x : num) {
             int temp = currMax;
-            currMax = Math.max(preMax+x,currMax);
+            currMax = Math.max(preMax + x, currMax);
             preMax = temp;
         }
         return currMax;
     }
 
-    class Solution {
-        void dfs(char[][] grid, int r, int c) {
-            int nr = grid.length;
-            int nc = grid[0].length;
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
 
-            if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-                return;
-            }
-
-            grid[r][c] = '0';
-            dfs(grid, r - 1, c);
-            dfs(grid, r + 1, c);
-            dfs(grid, r, c - 1);
-            dfs(grid, r, c + 1);
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
         }
 
-        public int numIslands(char[][] grid) {
-            if (grid == null || grid.length == 0) {
-                return 0;
-            }
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
 
-            int nr = grid.length;
-            int nc = grid[0].length;
-            int num_islands = 0;
-            for (int r = 0; r < nr; ++r) {
-                for (int c = 0; c < nc; ++c) {
-                    if (grid[r][c] == '1') {
-                        ++num_islands;
-                        dfs(grid, r, c);
+    /**
+     * 200. 岛屿数量
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    /**
+     * 200. 岛屿数量
+     * @param grid
+     * @return
+     */
+    public int numIslands1(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0'; // mark as visited
+                    Queue<Integer> neighbors = new LinkedList<>();
+                    neighbors.add(r * nc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            neighbors.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            neighbors.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            neighbors.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            neighbors.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
                     }
                 }
             }
-
-            return num_islands;
         }
 
+        return num_islands;
+    }
 
 
     public static void main(String[] args) {
@@ -1635,7 +1688,8 @@ public class LeetCode {
         listNode4.next = listNode6;
         listNode5.next = listNode6;
         int[] nums = {2, 3, -2, 0, 1, 6};
-        new LeetCode().maxProduct(nums);
+        char[][] grid = new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '0', '0'}, {'1', '0', '1'}};
+        new LeetCode().numIslands1(grid);
 
     }
 
