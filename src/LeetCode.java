35b3704a3db3b1e01ab10af58a00d35831482630
loyal888 <yunsong.hu@hand-china.com>
2019-10-24 09:22:18 +0800

    155. 最小栈

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1472,6 +1472,7 @@ public class LeetCode {
 
     /**
      * 152. 乘积最大子序列
+     *
      * @param nums
      * @return
      */
@@ -1490,6 +1491,39 @@ public class LeetCode {
         return res;
     }
 
+    /**
+     * 155. 最小栈
+     */
+    class MinStack {
+        private Stack<Integer> stack;
+        private Stack<Integer> min_stack;
+
+        public MinStack() {
+            stack = new Stack<>();
+            min_stack = new Stack<>();
+        }
+
+        public void push(int x) {
+            stack.push(x);
+            if (min_stack.isEmpty() || x <= min_stack.peek()) {
+                min_stack.push(x);
+            }
+        }
+
+        public void pop() {
+            if (stack.pop().equals(min_stack.peek())) {
+                min_stack.pop();
+            }
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return min_stack.peek();
+        }
+    }
 
     public static void main(String[] args) {
 //        String[] array = {"abc", "acb", "aq", "qa"};
