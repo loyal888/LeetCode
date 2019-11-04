3cb7adc4bb60c95fcd6aff4cea1f2016af355b70
loyal888 <yunsong.hu@hand-china.com>
2019-11-04 16:48:57 +0800

    大数之和

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1904,9 +1904,40 @@ public class LeetCode {
     }
 
     // =======================================分 割 线=========================================================
-    public static void main(String[] args) {
-        findMinNums(1231,2);
 
+    /**
+     * 两数相加(大数之和)
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int x = l1 == null?0:l1.val;
+            int y = l2 == null?0:l2.val;
+
+            int sum = x + y + carry;
+            carry = sum/10;
+            sum = sum%10;
+            cur.next = new ListNode(sum);
+            cur = cur.next;
+            if(l1 != null){l1 = l1.next;}
+            if(l2 != null){l2 = l2.next;}
+        }
+        if(carry == 1){
+            cur.next = new ListNode(1);
+        }
+        return pre.next;
+    }
+
+    // =======================================分 割 线=========================================================
+    public static void main(String[] args) {
+
+
+        findMinNums(1231,2);
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
@@ -1924,12 +1955,17 @@ public class LeetCode {
         ListNode listNode5 = new ListNode(1);
         ListNode listNode6 = new ListNode(8);
 
+
         listNode.next = listNode1;
         listNode1.next = listNode2;
-        listNode2.next = listNode3;
+//        listNode2.next = listNode3;
         listNode3.next = listNode4;
         listNode4.next = listNode6;
-        listNode5.next = listNode6;
+//        listNode5.next = listNode6;
+
+        addTwoNumbers(listNode,listNode3);
+
+
         int[] nums = {2, 3, -2, 0, 1, 6};
         char[][] grid = new char[][]{{'1', '1', '1'}, {'0', '1', '0'}, {'1', '0', '0'}, {'1', '0', '1'}};
         new LeetCode().reverseList(listNode);
