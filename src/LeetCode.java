3a3adb379af1e696e7ef2cb95adfc26965cdf115
loyal888 <yunsong.hu@hand-china.com>
2019-11-04 14:58:43 +0800

    674. 最长连续递增序列

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1722,6 +1722,29 @@ public class LeetCode {
     // =======================================分 割 线=========================================================
 
     /**
+     * 674. 最长连续递增序列
+     * @param nums
+     * @return
+     */
+    public int findLengthOfLCIS(int[] nums) {
+        if(nums.length <= 1)
+            return nums.length;
+        int ans = 1;
+        int count = 1;
+        for(int i=0;i<nums.length-1;i++) {
+            if(nums[i+1] > nums[i]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            ans = count > ans ? count : ans;
+        }
+        return ans;
+    }
+
+    // =======================================分 割 线=========================================================
+
+    /**
      * 队列Queue的链表实现（FIFO）
      * @param <String>
      */
