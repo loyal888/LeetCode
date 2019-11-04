ed05ab8ecd517ecf250ef26ce7fe194bce572449
loyal888 <yunsong.hu@hand-china.com>
2019-11-04 17:22:26 +0800

    83. 删除排序链表中的重复元素

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1995,6 +1995,24 @@ public class LeetCode {
             return s1.isEmpty();
         }
     }
+    // =======================================分 割 线=========================================================
+
+    /**
+     * 83. 删除排序链表中的重复元素
+     * @param head
+     * @return
+     */
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.next.val == current.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return head;
+    }
 
 
     // =======================================分 割 线=========================================================
