f8512dc8e665d96b9c4ed1600249f49003dad0db
loyal888 <yunsong.hu@hand-china.com>
2019-11-04 15:30:12 +0800

    108. 将有序数组转换为二叉搜索树

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1796,6 +1796,32 @@ public class LeetCode {
     }
 
     // =======================================分 割 线=========================================================
+
+    /**
+     * 108. 将有序数组转换为二叉搜索树
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        Arrays.sort(nums);
+        return sortedArrayToBST(nums, 0, nums.length);
+    }
+    private TreeNode sortedArrayToBST(int[] nums,int start,int end){
+        if(start == end){return null;}
+        int mid = (start + end ) >>> 1;
+
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = sortedArrayToBST(nums,start,mid);
+        root.right = sortedArrayToBST(nums,mid+1,end);
+        return root;
+    }
+
+
+
+
+
+    // =======================================分 割 线=========================================================
+
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
