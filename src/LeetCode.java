399bd7d715da782ebc4283765b29bb387347596f
loyal888 <yunsong.hu@hand-china.com>
2019-11-08 19:44:58 +0800

    208. 实现 Trie (前缀树)

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2338,9 +2338,9 @@ public class LeetCode {
         return count == 3;
     }
     // =======================================分 割 线=========================================================
+
     /**
      * 26. 删除排序数组中的重复项
-     *
      */
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) {
@@ -2355,13 +2355,100 @@ public class LeetCode {
         }
         return i + 1;
     }
-    // =======================================分 割 线=========================================================
 
+    // =======================================分 割 线=========================================================
+    static class TrieNode {
+
+        // R links to node children
+        private TrieNode[] links;
+
+        private final int R = 26;
+
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+    }
+
+    static class Trie {
+        private TrieNode root;
+
+        public Trie() {
+            root = new TrieNode();
+        }
+
+        // Inserts a word into the trie.
+        public void insert(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char currentChar = word.charAt(i);
+                if (!node.containsKey(currentChar)) {
+                    node.put(currentChar, new TrieNode());
+                }
+                node = node.get(currentChar);
+            }
+            node.setEnd();
+        }
+
+        private TrieNode searchPrefix(String word) {
+            TrieNode node = root;
+            for (int i = 0; i < word.length(); i++) {
+                char curLetter = word.charAt(i);
+                if (node.containsKey(curLetter)) {
+                    node = node.get(curLetter);
+                } else {
+                    return null;
+                }
+            }
+            return node;
+        }
+
+        // Returns if the word is in the trie.
+        public boolean search(String word) {
+            TrieNode node = searchPrefix(word);
+            return node != null && node.isEnd();
+        }
+
+        public boolean startsWith(String prefix) {
+            TrieNode node = searchPrefix(prefix);
+            return node != null;
+        }
+    }
 
 
     // =======================================分 割 线=========================================================
     public static void main(String[] args) {
-        int[] num = {0,0,1,1,1,2,2,3,3,4};
+        Trie trie = new Trie();
+        trie.insert("le");
+        trie.insert("leet");
+        trie.insert("code");
+        trie.search("co");
+        trie.search("le");
+        trie.search("leet");
+        trie.search("code");
+
+        int[] num = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         removeDuplicates(num);
         permute(num);
 
