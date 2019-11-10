274e9ad06e5eeca7273bb9195c82d01584dca5e0
loyal888 <yunsong.hu@hand-china.com>
2019-11-10 15:37:14 +0800

    279. 完全平方数

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2556,10 +2556,32 @@ public class LeetCode {
         }
         return false;
     }
+    // =======================================分 割 线=========================================================
+
+    /**
+     * 279. 完全平方数
+     * @param n
+     * @return
+     */
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1]; // 默认初始化值都为0
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // 最坏的情况就是每次+1
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 动态转移方程
+            }
+        }
+        return dp[n];
+    }
+
+
+
 
 
     // =======================================分 割 线=========================================================
     public static void main(String[] args) {
+        numSquares(10);
+
         char[][] matrix = new char[][]{
                 {'0', '1', '1', '1', '0'},
                 {'1', '1', '1', '1', '1'},
