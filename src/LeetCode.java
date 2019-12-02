aa4aaacf660842ee3e6915c72c4b71a84e299d2c
huyunsong <huyunsong@58.com>
2019-12-02 10:01:20 +0800

    38. 报数

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2755,10 +2755,46 @@ public class LeetCode {
         return left;
     }
 
+// ================================================================================
+    /**
+     * 38. 报数
+     * @param n
+     * @return
+     */
+    public static String countAndSay(int n) {
+        String res = "1";
+
+        for (int i = 1; i < n; i++) {
+            res = next_time(res);
+        }
+        return res;
+
+
+    }
+
+    private static String next_time(String res) {
+        int i = 0;
+        int n = res.length();
+        String ans = "";
+        while (i < n) {
+            int count = 1;
+            while ((i < n - 1) && (res.charAt(i) == res.charAt(i + 1))) {
+                i++;
+                count++;
+            }
+            ans += (count + "" + res.charAt(i));
+            i++;
+
+        }
+        return ans;
+    }
+    // ================================================================================
+
 
     // =======================================分 割 线=========================================================
     public static void main(String[] args) {
 
+        countAndSay(5);
         int result = divide(7, -2);
         lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18});
 
