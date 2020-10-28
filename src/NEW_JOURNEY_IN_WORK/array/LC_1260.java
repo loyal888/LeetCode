8778edec67166d30f26f03f8836d1f8b68e18d2f
loyal888 <pinecloud@163.com>
2020-10-28 22:59:10 +0800

    LC_1260

---
+++ src/NEW_JOURNEY_IN_WORK/array/LC_1260.java
@@ -0,0 +1,34 @@
+package NEW_JOURNEY_IN_WORK.array;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LC_1260 {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+
+        // Repeat the transform k times.
+        for (;k > 0; k--) {
+
+            int previous = grid[grid.length - 1][grid[0].length - 1];
+            for (int row = 0; row < grid.length; row++) {
+                for (int col = 0; col < grid[0].length; col++) {
+                    int temp = grid[row][col];
+                    grid[row][col] = previous;
+                    previous = temp;
+                }
+            }
+        }
+
+        // Copy the grid into a list for returning.
+        List<List<Integer>> result = new ArrayList<>();
+        for (int[] row : grid) {
+            List<Integer> listRow = new ArrayList<>();
+            result.add(listRow);
+            for (int v : row) listRow.add(v);
+        }
+
+        return result;
+    }
+}
